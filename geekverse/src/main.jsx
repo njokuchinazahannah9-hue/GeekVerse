8bc570c7588c3d0b317aa3fcfcadadaa9e0de1d6
njokuchinazahannah9-hue <njokuchinazahannah9@gmail.com>
2026-08-02 22:01:17 +0100

    Clean push with corrected imports

--- geekverse/src/main.jsx
+++ geekverse/src/main.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
-
 import App from "./App";
-
 import { CartProvider } from "./context/CartContext";
 import { WishlistProvider } from "./context/WishlistContext";
 import { UserProvider } from "./context/UserContext";
@@ -13,8 +10,6 @@ import "./styles/dailyReward.css";
 import "./styles/upcoming.css";
 import "./styles/settings.css";
 import "./styles/comics.css";
-
-
 import "./index.css";
 import "./styles/global.css";
 import "./styles/sidebar.css";
@@ -22,15 +17,15 @@ import "./styles/navbar.css";
 import "./styles/hero.css";
 import "./styles/category.css";
 import "./styles/trending.css";
-import "./styles/rightsidebar.css";
-import "./styles/dealcard.css";
-import "./styles/toppicks.css";
+import "./styles/rightSidebar.css";
+import "./styles/DealCard.css";
+import "./styles/TopPicks.css";
 import "./styles/premiumcard.css";
-import "./styles/whatsnew.css";
-import "./styles/specialOffer.css";
+import "./styles/WhatsNew.css";
+import "./styles/SpecialOffer.css";
 import "./styles/MovieCard.css";
-import "./styles/moviedetails.css";
-import "./styles/cart.css";
+import "./styles/MovieDetails.css";
+import "./styles/Cart.css";
 import "./styles/notifications.css";
 import "./styles/profile.css";
 import "./styles/profile2.css";
